803645a99046131a384590befee5bdd442a41bac
mahmoudsarmady <mhamad@sarmady.net>
2022-04-16 21:31:09 +0200

    rename group

---
+++ FlickerSearch/VIPER/SearchImageResult/Components/ActivityFooter/ActivityFooter.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityFooter.swift
+//  FlickerSearch
+//
+//  Created by Mahmoud Hamad on 16/04/2022.
+//
+
+import UIKit
+
+class ActivityFooter: UICollectionReusableView {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+}
